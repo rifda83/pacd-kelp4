04282397c5541a2079b55780d88f1759f2e46dc7
fujimotoAr <theofilus.arkhi@mail.ugm.ac.id>
2025-09-29 20:19:05 +0700

    change to xgboost

--- dataset.py
+++ dataset.py
@@ -2,39 +2,13 @@ import os, cv2, numpy as np, pandas as pd, matplotlib.pyplot as plt
 
 kernel_sharpen = np.array([[-1, -1, -1],
                     [-1, 8, -1],
-                    [-1, -1, -1]])*-1
+                    [-1, -1, -1]])
 
-def multiplication(kernel, submatrix):
-    kernel_size = len(kernel)
-    sum = 0
-    for i in range(kernel_size):
-        for j in range(kernel_size):
-            sum += kernel[i][j]*submatrix[i][j]
-    return sum
+kernel_dilation = np.ones((5, 5), np.uint8)
 
-def convolution(kernel, matrix):
-    kernel_size = len(kernel)
-    padded = np.zeros((len(matrix)+2, len(matrix[0])+2))
-    ret = np.zeros_like(padded)
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            padded[i+1][j+1] = matrix[i][j]
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            minplus = kernel_size//2
-            start_y = i+1-minplus
-            start_x = j+1-minplus
-            ret[i+1][j+1] = multiplication(
-                kernel, 
-                [arr[start_x:start_x+kernel_size] for arr in padded[start_y:start_y+kernel_size]]
-                )
-    return np.round(ret)
-            
 
-def get_dataset():
-    path_normal = "TB_Chest_Radiography_Database/Normal"
-    path_tb = "TB_Chest_Radiography_Database/Tuberculosis"
+def get_dataset(path_normal = "TBX11K/imgs/combined_normal", path_tb = "TBX11K/imgs/combined_tb"):
     x_normal=[]
     x_tb=[]
     for i, img in enumerate(os.listdir(path_normal)):        
@@ -53,7 +27,10 @@ def get_dataset():
         image = cv2.resize(image, (512, 512))
         # image = convolution(kernel_sharpen, image)
         image = abs(image+cv2.filter2D(image,-1,kernel_sharpen))
-
+        image = cv2.bitwise_not(image)
+        image = cv2.dilate(image, kernel_dilation, iterations=4)
+        image = cv2.erode(image, kernel_dilation, iterations=1)
+        image = cv2.copyMakeBorder(image, 2,2, 2, 2, borderType=cv2.BORDER_CONSTANT)
         x_normal.append(image)
     
     for i, img in enumerate(os.listdir(path_tb)):
@@ -75,57 +52,16 @@ def get_dataset():
             continue
         # image = convolution(kernel_sharpen, image)
         image = abs(image+cv2.filter2D(image,-1,kernel_sharpen))
+        image = cv2.bitwise_not(image)
+        image = cv2.dilate(image, kernel_dilation, iterations=4)
+        image = cv2.erode(image, kernel_dilation, iterations=1)
+        image = cv2.copyMakeBorder(image, 2,2, 2, 2, borderType=cv2.BORDER_CONSTANT)
 
         x_tb.append(image)
 
     return x_normal, x_tb
 
 def get_dataset_test():
-    path_normal = "TBX11K/imgs/health"
-    path_tb = "TBX11K/imgs/tb"
-    x_normal=[]
-    x_tb=[]
-    for i, img in enumerate(os.listdir(path_normal)):        
-        image = cv2.imread(os.path.join(path_normal, img))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Log transform
-        image = (image)/255
-        c = 255 / (1+np.max(image))
-        image = c*np.log(1+image)
-        image = np.array(image, dtype=np.uint8)
-
-        image = cv2.equalizeHist(image)
-
-        # image = cv2.equalizeHist(image)
-        image = cv2.resize(image, (512, 512))
-        # image = convolution(kernel_sharpen, image)
-        image = abs(image+cv2.filter2D(image,-1,kernel_sharpen))
-
-        x_normal.append(image)
-    
-    for i, img in enumerate(os.listdir(path_tb)):
-
-        image = cv2.imread(os.path.join(path_tb, img))
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Log transform
-        image = (image)/255
-        c = 255 / (1+np.max(image))
-        image = c*np.log(1+image)
-        image = np.array(image, dtype=np.uint8)
-
-        image = cv2.equalizeHist(image)
-
-
-        # image = cv2.equalizeHist(image)
-        image = cv2.resize(image, (512, 512))
-        if image.shape != (512, 512):
-            continue
-        # image = convolution(kernel_sharpen, image)
-        image = abs(image+cv2.filter2D(image,-1,kernel_sharpen))
-
-        x_tb.append(image)
-
-    return x_normal, x_tb
-    
+    path_normal = "TB_Chest_Radiography_Database/Normal"
+    path_tb = "TB_Chest_Radiography_Database/Tuberculosis"
+    return get_dataset(path_normal, path_tb)
--- segmentation.py
+++ segmentation.py
@@ -1,6 +1,88 @@
 import cv2, numpy as np
+from collections import deque
+def bfs(matrix, y, x, dist = 0, visited=None):
 
+    q=deque()
+    q.append((y,x,dist))
+    SIZE = 0
+    while len(q)!=0:
+        y, x, dist= q.popleft()
+        if(y>=len(matrix) or x >=len(matrix[0])):
+            continue
+        if(y<0 or x<0):
+            continue
+        if(matrix[y][x] != 255):
+            continue
+        SIZE+=1
+        delta_y = [0, 1, 0, -1]
+        delta_x = [1, 0, -1, 0]
+
+        for i, _ in enumerate(delta_y):
+            new_y = y+delta_y[i]
+            new_x = x+delta_x[i]
+            if(new_y>=len(matrix) or new_x >=len(matrix[0])):
+                continue
+            if(new_y<0 or new_x<0):
+                continue
+            if visited[new_y][new_x] == False:
+                visited[new_y][new_x]=True
+                q.append((new_y,new_x,dist+1))
+    return SIZE
+def fill(matrix, y, x, fill=0, dist = 0, visited=None):
+
+    q=deque()
+    q.append((y,x,dist))
+    SIZE = 0
+    while len(q)!=0:
+        y, x, dist= q.popleft()
+        if(y>=len(matrix) or x >=len(matrix[0])):
+            continue
+        if(y<0 or x<0):
+            continue
+        if(matrix[y][x] == 0):
+            continue
+        matrix[y][x] = fill
+        delta_y = [0, 1, 0, -1]
+        delta_x = [1, 0, -1, 0]
+
+        for i, _ in enumerate(delta_y):
+            new_y = y+delta_y[i]
+            new_x = x+delta_x[i]
+            if(new_y>=len(matrix) or new_x >=len(matrix[0])):
+                continue
+            if(new_y<0 or new_x<0):
+                continue
+            if visited[new_y][new_x] == False:
+                visited[new_y][new_x]=True
+                q.append((new_y,new_x,dist+1))
+    return matrix
+def fill_white_areas(image):
+    white_areas=[]
+    visited = [[False]*len(image[0]) for i in range(len(image))]
+
+    for i, item_i in enumerate(image):
+        for j, item_j in enumerate(item_i):
+            if item_j==255 and not visited[i][j]:
+                white_areas.append((bfs(image,i,j, visited=visited), (i,j)))
+    white_areas = sorted(white_areas)
+    visited = [[False]*len(image[0]) for i in range(len(image))]
     
+    for a,b in white_areas[:-2]:
+        y, x = b
+        image = fill(image, y, x, visited=visited, fill=0)
+    return image
 def get_segmentation(source): 
+
+    # ret3,th3 = cv2.threshold(source,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     ret3,th3 = cv2.threshold(source,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    contours, hierarchy = cv2.findContours(image=th3, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_SIMPLE)
+                                      
+    cv2.drawContours(image=th3, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=10, lineType=cv2.LINE_AA)
+    # th3 = cv2.adaptiveThreshold(source,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+    #         cv2.THRESH_BINARY,11,2)
+    # blur = cv2.GaussianBlur(source,(5,5),0)
+    # ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    # th3=fill_white_areas(th3)
+    
     return th3
