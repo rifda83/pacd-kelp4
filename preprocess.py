cd34857252589d20d7a38187073c0435e4b3a7ac
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2025-12-01 11:11:16 +0700

    use new preprocess and segmentation technique

--- preprocess.py
+++ preprocess.py
@@ -1,15 +1,27 @@
 import os, cv2, numpy as np, pandas as pd, matplotlib.pyplot as plt
 
+kernel_sharpen = np.array([[-1, -1, -1],
+                    [-1, 8, -1],
+                    [-1, -1, -1]])
+
+kernel_dilation = np.ones((5, 5), np.uint8)
+
 def preprocess(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Log transform
     image = (image)/255
     c = 255 / (1+np.max(image))
     image = c*np.log(1+image)
     image = np.array(image, dtype=np.uint8)
 
-    ycrcb_img = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
-    ycrcb_img[:, :, 0] = cv2.equalizeHist(ycrcb_img[:, :, 0])
-    image = cv2.cvtColor(ycrcb_img, cv2.COLOR_YCrCb2BGR)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.equalizeHist(image)
 
-    image = cv2.resize(image, (512, 512))
-    return image
+    # image = cv2.equalizeHist(image)
+    image = cv2.resize(image, (128, 128))
+    # image = convolution(kernel_sharpen, image)
+    image = abs(image+cv2.filter2D(image,-1,kernel_sharpen))
+    image = cv2.bitwise_not(image)
+    image = cv2.dilate(image, kernel_dilation, iterations=2)
+    image = cv2.erode(image, kernel_dilation, iterations=2)
+    image = cv2.copyMakeBorder(image, 2,2, 2, 2, borderType=cv2.BORDER_CONSTANT)
